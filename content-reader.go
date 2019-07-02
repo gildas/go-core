f837ae6a2842674ab36e8344ac897a1578248f96
Gildas Cherruel <gildas@breizh.org>
2019-07-03 03:11:17 +0900

    Added a core.Content

--- content-reader.go
+++ content-reader.go
@@ -1,12 +1,32 @@
 package core
 
 import (
+	"bytes"
 	"io"
+	"io/ioutil"
+	"net/url"
 )
+
+// Content defines some content
+type Content struct {
+	Type   string        `json:"contentType"`
+	URL    *url.URL      `json:"contentUrl"`
+	Length int           `json:"contentLength"`
+	Data   []byte        `json:"contentData"`
+}
 
 // ContentReader defines a content Reader (like data from an HTTP request)
 type ContentReader struct {
 	Type   string        `json:"contentType"`
 	Length int           `json:"contentLength"`
 	Reader io.ReadCloser `json:"-"`
+}
+
+// Reader gets a ContentReader from this Content
+func (content *Content) Reader() *ContentReader {
+	return &ContentReader{
+		Type:   content.Type,
+		Length: content.Length,
+		Reader: ioutil.NopCloser(bytes.NewReader(content.Data)),
+	}
 }
