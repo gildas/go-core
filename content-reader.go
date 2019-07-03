1cb9e1e626baf42b7a2c865444a6b9e38c8362eb
Gildas Cherruel <gildas@breizh.org>
2019-07-03 11:22:17 +0900

    add convenience constructors

--- content-reader.go
+++ content-reader.go
@@ -11,22 +11,56 @@ import (
 type Content struct {
 	Type   string        `json:"contentType"`
 	URL    *url.URL      `json:"contentUrl"`
-	Length int           `json:"contentLength"`
+	Length int64         `json:"contentLength"`
 	Data   []byte        `json:"contentData"`
 }
 
 // ContentReader defines a content Reader (like data from an HTTP request)
 type ContentReader struct {
 	Type   string        `json:"contentType"`
-	Length int           `json:"contentLength"`
+	Length int64         `json:"contentLength"`
 	Reader io.ReadCloser `json:"-"`
+}
+
+// ContentWithData instantiates a Content from a simple byte array
+func ContentWithData(data []byte, options... interface{}) *Content {
+	content := &Content{}
+	content.Data = data
+	for _, option := range options {
+		if u, ok := option.(*url.URL); ok {
+			content.URL = u
+		} else if t, ok := option.(string); ok {
+			content.Type = t
+		} else if l, ok := option.(int64); ok  && l > 0 {
+			content.Length = l
+		} else if l, ok := option.(int); ok  && l > 0 {
+			content.Length = int64(l)
+		}
+	}
+	if content.Length == 0 {
+		content.Length = int64(len(content.Data))
+	}
+	return content
+}
+
+// ContentFromReader instantiates a Content from an I/O reader
+func ContentFromReader(reader io.Reader, options... interface{}) (*Content, error) {
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return ContentWithData(data, options...), nil
 }
 
 // Reader gets a ContentReader from this Content
 func (content *Content) Reader() *ContentReader {
-	return &ContentReader{
+	reader := &ContentReader{
 		Type:   content.Type,
 		Length: content.Length,
 		Reader: ioutil.NopCloser(bytes.NewReader(content.Data)),
 	}
+	if reader.Length == 0 {
+		reader.Length = int64(len(content.Data))
+	}
+	return reader
 }
