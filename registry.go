9a76507d571c8050234b0085e71f54f6a4ae1c38
Gildas Cherruel <gildas@breizh.org>
2020-11-09 14:06:39 +0900

    Added TypeRegistry

---
+++ registry.go
@@ -0,0 +1,16 @@
+package line
+
+import (
+	"reflect"
+)
+
+// TypeRegistry contains a map of identifier vs Type
+type TypeRegistry map[string]reflect.Type
+
+// Add adds one or more TypeCarriers to the TypeRegistry
+func (registry TypeRegistry) Add(classes ...core.TypeCarrier) TypeRegistry {
+	for _, class := range classes {
+		registry[class.GetType()] = reflect.TypeOf(class)
+	}
+	return registry
+}
