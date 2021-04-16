d8fa837719ba1f30906ce6808b24371e9cbf5c27
Gildas Cherruel <gildas@breizh.org>
2021-04-17 04:31:07 +0900

    Added TypeRegistry Unmarshaling of payloads

--- registry.go
+++ registry.go
@@ -1,6 +1,9 @@
 package core
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"reflect"
 )
 
@@ -13,4 +16,26 @@ func (registry TypeRegistry) Add(classes ...TypeCarrier) TypeRegistry {
 		registry[class.GetType()] = reflect.TypeOf(class)
 	}
 	return registry
+}
+
+// UnmarshalJSON unmarshal a payload into a Type Carrier
+//
+// The interface that is returned contains a pointer to the TypeCarrier structure
+func (registry TypeRegistry) UnmarshalJSON(payload []byte) (interface{}, error) {
+	header := struct{Type string `json:"type"`}{}
+	if err := json.Unmarshal(payload, &header); err != nil {
+		return nil, err
+	}
+	if len(header.Type) == 0 {
+		return nil, errors.New(`Missing JSON Property "type"`)
+	}
+
+	if valueType, found := registry[header.Type]; found {
+		value := reflect.New(valueType).Interface()
+		if err := json.Unmarshal(payload, value); err != nil {
+			return nil, err
+		}
+		return value, nil
+	}
+	return nil, fmt.Errorf(`Unsupported Type "%s"`, header.Type)
 }
