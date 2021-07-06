ebc39289c3222e6a95071e786c732b380539961c
Gildas Cherruel <gildas@breizh.org>
2021-07-06 23:48:16 +0900

    TypeRegistry now accepts options type tags when unmarshaling

--- registry_test.go
+++ registry_test.go
@@ -11,6 +11,7 @@ import (
 
 type Something interface {
 	TypeCarrier
+	GetData() string
 }
 
 type Something1 struct {
@@ -25,8 +26,16 @@ func (s Something1) GetType() string {
 	return "something1"
 }
 
+func (s Something1) GetData() string {
+	return s.Data
+}
+
 func (s Something2) GetType() string {
 	return "something2"
+}
+
+func (s Something2) GetData() string {
+	return s.Data
 }
 
 func TestCanCreateTypeRegistry(t *testing.T) {
@@ -43,10 +52,25 @@ func TestCanUnmarshalTypeCarrier(t *testing.T) {
 	require.Nilf(t, err, "Failed to Unmarshal payload: %s", err)
 	require.NotNil(t, object, "Returned object cannot be nil")
 
-	value, ok := object.(*Something1)
+	value, ok := object.(Something)
 	require.True(t, ok, "Return object should carry a *Something1")
 	require.NotNil(t, value, "Actual value cannot be nil")
-	assert.Equal(t, "Hello", value.Data)
+	assert.Equal(t, "Hello", value.GetData())
+}
+
+func TestCanUnmarshalTypeCarrierWithTypetag(t *testing.T) {
+	registry := TypeRegistry{}.Add(Something1{}, Something2{})
+	require.Equal(t, 2, len(registry))
+
+	payload := []byte(`{"__type": "something1", "data": "Hello"}`)
+	object, err := registry.UnmarshalJSON(payload, "__type")
+	require.Nilf(t, err, "Failed to Unmarshal payload: %s", err)
+	require.NotNil(t, object, "Returned object cannot be nil")
+
+	value, ok := object.(Something)
+	require.True(t, ok, "Return object should carry a *Something1")
+	require.NotNil(t, value, "Actual value cannot be nil")
+	assert.Equal(t, "Hello", value.GetData())
 }
 
 func TestShouldFailUnmarshalingTypeCarrierWithoutType(t *testing.T) {
@@ -73,13 +97,18 @@ func TestShouldFailUnmarshalingTypeCarrierWithInvalidJSON(t *testing.T) {
 	registry := TypeRegistry{}.Add(Something1{}, Something2{})
 	require.Equal(t, 2, len(registry))
 
-	payload := []byte(`{"type": 2, "data": "Hello"}`)
+	payload := []byte(`{"type": 2", "data": "Hello"}`)
 	_, err := registry.UnmarshalJSON(payload)
 	require.NotNil(t, err)
-	assert.Equal(t, "json: cannot unmarshal number into Go struct field .type of type string", err.Error())
+	assert.Equal(t, "invalid character '\"' after object key:value pair", err.Error())
+}
 
-	payload = []byte(`{"type": "something1", "data": 2}`)
-	_, err = registry.UnmarshalJSON(payload)
+func TestShouldFailUnmarshalingTypeCarrierWithInvalidJSON2(t *testing.T) {
+	registry := TypeRegistry{}.Add(Something1{}, Something2{})
+	require.Equal(t, 2, len(registry))
+
+	payload := []byte(`{"type": "something1", "data": 2}`)
+	_, err := registry.UnmarshalJSON(payload)
 	require.NotNil(t, err)
 	assert.Equal(t, "json: cannot unmarshal number into Go struct field Something1.data of type string", err.Error())
 }
