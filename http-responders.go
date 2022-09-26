5847eadeb09cfbc8220e9e607154dceaf3a0b478
Gildas Cherruel <gildas@breizh.org>
2022-09-26 23:20:17 +0900

    New HTTP Responders

--- http-responders.go
+++ http-responders.go
@@ -2,9 +2,12 @@ package core
 
 import (
 	"encoding/json"
+	"html/template"
 	"net/http"
 	"reflect"
 	"strconv"
+
+	"github.com/google/uuid"
 )
 
 // RespondWithError will send a reply with an error as JSON and a HTTP Status code
@@ -31,15 +34,12 @@ func RespondWithError(w http.ResponseWriter, code int, err error) {
 		props["what"] = field.String()
 	}
 	if field = errValue.FieldByName("Value"); field.IsValid() {
-		switch field.Type().Kind() {
-		case reflect.String:
-			if field.Len() > 0 {
-				props["value"] = field.String()
-			}
-		case reflect.Interface, reflect.Ptr:
-			if !field.IsNil() {
-				props["value"] = field.String()
-			}
+		if stringer, ok := field.Interface().(interface{ String() string }); ok {
+			props["value"] = stringer.String()
+		} else if identifiable, ok := field.Interface().(interface{ GetID() uuid.UUID }); ok {
+			props["value"] = identifiable.GetID().String()
+		} else if field.Type().Kind().String() == "string" && field.Len() > 0 {
+			props["value"] = field.String()
 		}
 	}
 	RespondWithJSON(w, code, props)
@@ -50,5 +50,14 @@ func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
-	w.Write(response)
+	_, _ = w.Write(response)
+}
+
+// RespondWithHTMLTemplate will send a reply with a HTML payload generated from an HTML Template and a HTTP Status code
+func RespondWithHTMLTemplate(w http.ResponseWriter, code int, template *template.Template, name string, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := template.ExecuteTemplate(w, name, data); err != nil {
+		RespondWithError(w, http.StatusInternalServerError, err)
+		return
+	}
 }
