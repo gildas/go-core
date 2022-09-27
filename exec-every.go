a190980ba8c5a5862b9f873a722c90b0b209e333
Gildas Cherruel <gildas@breizh.org>
2022-09-27 23:41:56 +0900

    Do not execute job in its own go subroutine

--- exec-every.go
+++ exec-every.go
@@ -5,11 +5,17 @@ import (
 )
 
 // ExecEvery runs a func as a go routine regularly
+//
 // returns 3 chans:
+//
 // - a chan to stop the time loop, pipe true
+//
 // - a chan to force the job execution on demand
+//
 // - a chan to change the ticker interval
+//
 // the invoked func can modify the interval by piping a new Duration to its given chan
+//
 // the func is executed once before the ticker starts
 func ExecEvery(job func(tick int64, at time.Time, changeme chan time.Duration), every time.Duration) (chan bool, chan bool, chan time.Duration) {
 	changeme := make(chan time.Duration)
@@ -18,7 +24,7 @@ func ExecEvery(job func(tick int64, at time.Time, changeme chan time.Duration), 
 
 	go func() {
 		tick := int64(0)
-		go job(tick, time.Now(), changeme)
+		job(tick, time.Now(), changeme)
 		ticker := time.NewTicker(every)
 		for {
 			select {
@@ -28,10 +34,10 @@ func ExecEvery(job func(tick int64, at time.Time, changeme chan time.Duration), 
 					ticker = time.NewTicker(newtick)
 				}
 			case <-pingme:
-				go job(tick, time.Now(), changeme)
+				job(tick, time.Now(), changeme)
 			case now := <-ticker.C:
 				tick++
-				go job(tick, now, changeme)
+				job(tick, now, changeme)
 			case <-stopme:
 				ticker.Stop()
 				return
