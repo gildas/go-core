da1b953472e683048ea13e2eebcd8eab74a33ae8
Gildas <gildas@breizh.org>
2023-05-12 17:50:18 +0900

    Go fmt (new comment format)

--- doc.go
+++ doc.go
@@ -1,14 +1,12 @@
 /*
 This package contains various helpers for the Go language
 
-TypeRegistry
+# TypeRegistry
 
 The TypeRegistry can be used to unmarshal JSON data into interfaces.
 
 The interface members must implement core.TypeCarrier.
 
 See the example in #TypeRegistry.UnmarshalJSON
-
-
 */
 package core
--- map.go
+++ map.go
@@ -5,10 +5,11 @@ package core
 // Note: The result is a new slice, the original is not modified
 //
 // Example:
-// 	// Map all numbers in a slice to their square
-// 	squares := Map(numbers, func(number int) int {
-// 		return number * number
-// 	})
+//
+//	// Map all numbers in a slice to their square
+//	squares := Map(numbers, func(number int) int {
+//		return number * number
+//	})
 func Map[T any, R any](items []T, mapper func(T) R) (result []R) {
 	for _, item := range items {
 		result = append(result, mapper(item))
