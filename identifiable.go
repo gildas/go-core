06733b58a6a32c2c14221e2f9ae5138eb90fb401
Gildas Cherruel <gildas@breizh.org>
2024-11-19 08:58:31 +0900

    Added generic equals/matches

--- identifiable.go
+++ identifiable.go
@@ -11,3 +11,27 @@ type Identifiable interface {
 type StringIdentifiable interface {
 	GetID() string
 }
+
+// EqualIdentifiable compares two values for equality by their Identifier
+func EqualIdentifiable[T Identifiable](a, b T) bool {
+	return a.GetID() == b.GetID()
+}
+
+// MatchIdentifiable returns a function that matches an Identifiable value
+func MatchIdentifiable[T Identifiable](search T) func(T) bool {
+	return func(item T) bool {
+		return item.GetID() == search.GetID()
+	}
+}
+
+// EqualStringIdentifiable compares two values for equality by their Identifier
+func EqualStringIdentifiable[T StringIdentifiable](a, b T) bool {
+	return a.GetID() == b.GetID()
+}
+
+// MatchStringIdentifiable returns a function that matches a StringIdentifiable value
+func MatchStringIdentifiable[T StringIdentifiable](search T) func(T) bool {
+	return func(item T) bool {
+		return item.GetID() == search.GetID()
+	}
+}
--- named.go
+++ named.go
@@ -4,3 +4,15 @@ package core
 type Named interface {
 	GetName() string
 }
+
+// RqualNamed compares two values for equality by name
+func EqualNamed[T Named](a, b T) bool {
+	return a.GetName() == b.GetName()
+}
+
+// MatchNamed returns a function that matches a Named value
+func MatchNamed[T Named](search T) func(T) bool {
+	return func(item T) bool {
+		return item.GetName() == search.GetName()
+	}
+}
